96f287e8c4cd449096aea0281221f7a7cf952c56
hafidhiansya <87061613+hafidhiansya@users.noreply.github.com>
2021-10-11 22:11:14 +0700

    Add files via upload

---
+++ KomisiKaryawanTes/KomisiKaryawanTes/KomisiKaryawan.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KomisiKaryawanTes
+{
+    public class KomisiKaryawan : object
+    {
+        public string NamaDepan { get; }
+        public string NamaBelakang { get; }
+        public string NoJaminanSosial { get; }
+        private decimal penjualanKotor;
+        private decimal presentaseKomisi;
+
+        public KomisiKaryawan (string namaDepan, string namaBelakang,
+            string noJaminanSosial, decimal penjualanKotor,
+            decimal presentaseKomisi)
+        {
+            NamaDepan = namaDepan;
+            NamaBelakang = namaBelakang;
+            NoJaminanSosial = noJaminanSosial;
+            PenjualanKotor = penjualanKotor;
+            PresentaseKomisi = presentaseKomisi;
+        }
+
+        public decimal PenjualanKotor
+        {
+            get
+            {
+                return penjualanKotor;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value),
+                        value, $"{nameof(PenjualanKotor)} harus >= 0");
+                }
+                penjualanKotor = value;
+            }
+        }
+        public decimal PresentaseKomisi
+        {
+            get
+            {
+                return presentaseKomisi;
+            }
+            set
+            {
+                if (value <= 0 || value >= 1)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value),
+                        value, $"{nameof(PresentaseKomisi)} harus > 0 dan < 1");
+                }
+                presentaseKomisi = value;
+            }
+        }
+
+        public decimal Pendapatan() => presentaseKomisi * penjualanKotor;
+
+        public override string ToString() =>
+            $"Komisi Karyawan       : {NamaDepan} {NamaBelakang}\n" +
+            $"Nomor Jaminan Sosial  : {NoJaminanSosial}\n" +
+            $"Penjualan Kotor       : {penjualanKotor:C}\n" +
+            $"Tingkat Komisi        : {presentaseKomisi:F2}";
+        
+    }
+}
---
+++ KomisiKaryawanTes/KomisiKaryawanTes/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace KomisiKaryawanTes
+{
+    class Program
+    {
+        static void Main()
+        {
+            var karyawan = new KomisiKaryawan("Dede", "Sunandar",
+                "325-346-3456", 10000.00M, .06M);
+
+            Console.WriteLine("Informasi karyawan diperoleh dengan properti dan metode: \n");
+            Console.WriteLine($"Nama depan          : { karyawan.NamaDepan}");
+            Console.WriteLine($"Nama belakang       : {karyawan.NamaBelakang}");
+            Console.WriteLine($"No Jaminan Sosial   : { karyawan.NoJaminanSosial}");
+            Console.WriteLine($"Penjualan kotor     : {karyawan.PenjualanKotor:C}");
+            Console.WriteLine($"Presentase komisi   : {karyawan.PresentaseKomisi:F2}");
+            Console.WriteLine($"Pendapatan          : {karyawan.Pendapatan():C}");
+
+            karyawan.PenjualanKotor = 5000.00M;
+            karyawan.PresentaseKomisi = .1M;
+
+            Console.WriteLine("\nInformasi karyawan yang diperbarui diperoleh oleh ToString:\n");
+            Console.WriteLine(karyawan);
+            Console.WriteLine($"Pendapatan: {karyawan.Pendapatan():C}");
+
+            Console.ReadLine();
+
+        }
+    }
+}
